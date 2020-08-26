efdf94ad140f874b49c1fac715310d07ef3af7b5
Vladimir <vovan986021@gmail.com>
2020-08-26 20:23:08 +0300

    add tests in DomainsControllerTest

--- tests/Feature/DomainsControllerTest.php
+++ tests/Feature/DomainsControllerTest.php
@@ -2,19 +2,32 @@
 
 namespace Tests\Feature;
 
+use Carbon\Carbon;
+use Faker\Generator;
+use Faker\Provider\Address;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class DomainsControllerTest extends TestCase
 {
     use DatabaseTransactions, DatabaseMigrations;
 
+    protected $id;
+    protected $domain = 'https://www.w3schools.com';
+
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->id = DB::table('domains')->insertGetId([
+            'name'       => $this->domain,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 
     public function testIndex()
@@ -25,14 +38,18 @@ class DomainsControllerTest extends TestCase
 
     public function testStore()
     {
-        $data = ['domain' => 'https://www.w3schools.com'];
-        $response = $this->post(route('domains.store'), $data);
+        $response = $this->post(route('domains.store'), ['domain' => $this->domain]);
 
         $response->assertSessionHasNoErrors();
         $response->assertRedirect();
 
-        $this->assertDatabaseHas('domains', ['name' => $data['domain']]);
+        $this->assertDatabaseHas('domains', ['name' => $this->domain]);
     }
 
+    public function testShow()
+    {
+        $response = $this->get(route('domains.show', $this->id));
 
+        $response->assertOk();
+    }
 }
