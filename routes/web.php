a09aa875d3d98215d10ce233ec60a96603d67b79
Vladimir <vovan986021@gmail.com>
2020-09-02 23:13:16 +0300

    add route in web

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', ['as' => 'index', 'uses' => 'IndexController@index']);
 
+//Route::resource('domain', 'DomainCheckController');
+Route::post('/domains/{id}/check', ['as' => 'check', 'uses' => 'DomainCheckController@store']);
 Route::resource('domains', 'DomainsController');
