baf83c02ccc53d10ed87c8efac034fc8e585e90b
Vladimir <vovan986021@gmail.com>
2020-09-04 23:08:38 +0300

    delete unnecessary route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', ['as' => 'index', 'uses' => 'IndexController@index']);
 
-//Route::resource('domain', 'DomainCheckController');
 Route::post('/domains/{id}/check', ['as' => 'check', 'uses' => 'DomainCheckController@store']);
 Route::resource('domains', 'DomainsController');
