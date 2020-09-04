1ddcec37934bdb09af2606e5756751728084b027
Vladimir <vovan986021@gmail.com>
2020-09-04 09:47:07 +0300

    add parser

--- app/Http/Controllers/DomainCheckController.php
+++ app/Http/Controllers/DomainCheckController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Carbon\Carbon;
+use DiDom\Document;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
@@ -24,10 +25,31 @@ class DomainCheckController extends Controller
             $domain_check_id = 0;
             if ($domain) {
                 $response = Http::get($domain['name']);
+                $html = $response->body();
+
+                $document = new Document($html);
+
+                $h1_element = $document->first('h1');
+                if (isset($h1_element)) {
+                    $h1 = $h1_element->text();
+                }
+
+                $meta_keywords_element = $document->first('meta[name=keywords]');
+                if (isset($meta_keywords_element)) {
+                    $keywords = $meta_keywords_element->getAttribute('content');
+                }
+
+                $meta_description_element = $document->first('meta[name=description]');
+                if (isset($meta_description_element)) {
+                    $description = $meta_description_element->getAttribute('content');
+                }
 
                 $domain_check_id = DB::table('domain_checks')->insertGetId([
                     'domain_id'   => $id,
                     'status_code' => $response->status(),
+                    'keywords'    => $keywords ?? null,
+                    'description' => $description ?? null,
+                    'h1'          => $h1 ?? null,
                     'created_at'  => Carbon::now('Europe/Moscow'),
                     'updated_at'  => Carbon::now('Europe/Moscow'),
                 ]);
