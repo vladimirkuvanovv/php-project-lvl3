7acdc8902f27372597c52c855a2f68c43fe05224
Vladimir <vovan986021@gmail.com>
2020-09-07 22:20:31 +0300

    update view

--- resources/views/domain.blade.php
+++ resources/views/domain.blade.php
@@ -6,25 +6,25 @@
         <div class="row ">
             <div class="col-12">
                 <div class="caption">
-                    <span>Site: {{ $domain['name'] }}</span>
+                    <span>Site: {{ $domain->name }}</span>
                 </div>
                 <table class="table table-dark table-striped table-bordered">
                     <tbody>
                     <tr>
                         <td>id</td>
-                        <td>{{ $domain['id'] }}</td>
+                        <td>{{ $domain->id }}</td>
                     </tr>
                     <tr>
                         <td>name</td>
-                        <td>{{ $domain['name'] }}</td>
+                        <td>{{ $domain->name }}</td>
                     </tr>
                     <tr>
                         <td>created_at</td>
-                        <td>{{ $domain['created_at'] }}</td>
+                        <td>{{ $domain->created_at }}</td>
                     </tr>
                     <tr>
                         <td>updated_at</td>
-                        <td>{{ $domain['updated_at'] }}</td>
+                        <td>{{ $domain->updated_at }}</td>
                     </tr>
                     </tbody>
                 </table>
