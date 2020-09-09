6aeece00d0739ced965cfb26bc327614a17be92c
Vladimir <vovan986021@gmail.com>
2020-09-09 14:48:01 +0300

    add footer in bottom page

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -14,49 +14,48 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-{{--    <link rel="stylesheet" href="css/bootstrap.css">--}}
     <link rel="stylesheet" href="/css/font-awesome.css">
     <link rel="stylesheet" href="/css/style.css">
 
 </head>
 <body>
-<!-- header -->
-<header class="home">
-    <!-- navbar -->
-@section('navbar')
-    <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-dark">
-
-            <a href="{{ route('index') }}" class="navbar-brand">
-                <h2>Analyzer</h2>
-            </a>
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo" aria-controls="navbarTogglerDemo" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div id="navbarTogglerDemo" class="collapse navbar-collapse">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a href="{{ route('index') }}" class="nav-link">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('domains.index') }}" class="nav-link">Domains</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </div>
-@show
-    <!-- end navbar -->
-</header>
-
 <div class="wrapper">
-<main class="flex-grow-1">
-    @yield('content')
-    @yield('additional-content')
-</main>
+    <!-- header -->
+    <header class="home">
+        <!-- navbar -->
+    @section('navbar')
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark">
 
+                <a href="{{ route('index') }}" class="navbar-brand">
+                    <h2>Analyzer</h2>
+                </a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo" aria-controls="navbarTogglerDemo" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div id="navbarTogglerDemo" class="collapse navbar-collapse">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a href="{{ route('index') }}" class="nav-link">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('domains.index') }}" class="nav-link">Domains</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    @show
+        <!-- end navbar -->
+    </header>
+
+    <main class="flex-grow-1">
+        @yield('content')
+        @yield('additional-content')
+    </main>
+</div>
 <!-- footer -->
 <footer>
     <div class="container">
@@ -66,6 +65,5 @@
     </div>
 </footer>
 <!-- end footer -->
-</div>
 </body>
 </html>
